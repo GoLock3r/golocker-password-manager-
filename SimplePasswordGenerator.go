94af8f794a3790123f907ebcd44bbdc503b716cf
nryan100 <nicholasryan100@gmail.com>
2022-02-03 14:34:27 -0700

    Testing

--- SimplePasswordGenerator.go
+++ SimplePasswordGenerator.go
@@ -1,10 +1,9 @@
 // This code was written by Hunter Holton for the Project GoLock3r intended purposes of this code are for the password
-//generation for the coming password manager integration. All of the code is subject to change based on user needs 
-// Code produces passwords containing letters of both upper and lowercase chunks of 4 seperated by dashes 
+//generation for the coming password manager integration. All of the code is subject to change based on user needs
+// Code produces passwords containing letters of both upper and lowercase chunks of 4 seperated by dashes
 // User has the option to have password contain special charaters including !@#$%^&*
-// Inclusivity of special characters depends on the websites that is used for what can be included 
-// further changes to what characters can be included will grow depending on what websites allow 
-
+// Inclusivity of special characters depends on the websites that is used for what can be included
+// further changes to what characters can be included will grow depending on what websites allow
 
 package main
 
@@ -30,18 +29,18 @@ func main() {
 	} else {
 		allow = false
 	}
-	for true { // this for loop runs off the true boolean it is meant to act as a while loop of the same idea as while true 
+	for true { // this for loop runs off the true boolean it is meant to act as a while loop of the same idea as while true
 		if num > 16 || num < 8 {
 			println("The number you entered doesnt meet our requierments please enter a number between 8 and 16")
 			fmt.Scanln(&num)
 		}
-		if num <= 16 && num >= 8 { // gaurentees that correct values sit between 8 and 16 this can be changed in the future depending on need for stronger passwords 
+		if num <= 16 && num >= 8 { // gaurentees that correct values sit between 8 and 16 this can be changed in the future depending on need for stronger passwords
 			break // breaks out of the defined infinite loop
 		}
 	}
 	var pw = passwordgen(num, allow)
 
-	print(pw) // outputs password that was created at random 
+	print(pw) // outputs password that was created at random
 
 }
 
@@ -118,4 +117,3 @@ func passwordgen(n int, allow bool) string {
 
 	}
 }
-
