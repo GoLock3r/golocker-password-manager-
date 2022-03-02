071fe0e3ab4e8f6b258fd5fdaa2dd0ada0d4b9bb
hunter-holton <hunter.holton@me.com>
2022-03-02 11:18:39 -0700

    skeleton code of testing for password generator

---
+++ pkg/generator/password-generator_test.go
@@ -0,0 +1,21 @@
+package generator
+
+import(
+	"golock3r/server/logger"
+	"os"
+	"strings"
+	"testing"
+)
+
+func test_zero_password(t *testing.T){
+
+}
+func test_outofsize_password(t *testing.T){
+
+}
+func test_special_char(t *testing.T){
+
+}
+func test_nonspecial_char(t *testing.T){
+	
+}
