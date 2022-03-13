063723545caeceb7e53a55d94cda0ef5a54f8bb1
hunter-holton <hunter.holton@me.com>
2022-03-12 18:25:17 -0700

    Has everything but contains dictionary word

--- pkg/generator/password-strength-test.go
+++ pkg/generator/password-strength-test.go
@@ -1,43 +1,50 @@
 package generator
 
-import(
+import (
 	"strings"
 )
 
-func passwordstren(password string){ 
-	var strength string 
+func passwordstren(password string) {
+	var strength string
 	var length int
 	var hascap int
 	var hasspec int
-	var hasnum int 
-	var hasnodicword int 
-	
-	if(len(password) > 8){
+	var hasnum int
+	var hasnodicword int
+
+	if len(password) > 8 {
 		length = 1
-	}else { 
+	} else {
 		length = 0
 	}
 
-	for i := 0; i < len(password); i ++ { 
-		if(password[i].iscapital()){//psuedo my boy
-				hascap = 1;
-
-			}
-		if(password[i].isspecial()){
+	special_chars := []string{"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
+	numbers := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
+	capLet := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	for _, c := range special_chars {
+		if strings.Contains(password, c) {
 			hasspec = 1
 		}
-		if(password[i].isnum()){
+	}
+	for _, c := range numbers {
+		if strings.Contains(password, c) {
 			hasnum = 1
 		}
 	}
+	for _, c := range capLet {
+		c = strings.ToUpper(c)
+		if strings.Contains(password, c) {
+			hascap = 1
+		}
+	}
 
-	if(length + hascap + hasspec + hasnum + hasnodicword == 4){
+	if length+hascap+hasspec+hasnum+hasnodicword == 4 {
 		strength = "your password is decent"
 	}
-	if(length + hascap + hasspec + hasnum +hasnodicword <= 3){
+	if length+hascap+hasspec+hasnum+hasnodicword <= 3 {
 		strength = "your password sucks figure it out"
 	}
-	if(length + hascap + hasspec + hasnum + hasnodicword == 5 ){
+	if length+hascap+hasspec+hasnum+hasnodicword == 5 {
 		strength = "eyyyyy lets get it your password is strong"
 	}
 	return strength
