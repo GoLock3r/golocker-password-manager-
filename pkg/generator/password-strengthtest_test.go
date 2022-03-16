dcaf32806508c8286f7de40fae8b5f7ed394b866
hunter-holton <hunter.holton@me.com>
2022-03-16 14:29:47 -0600

    fixed password generator tests :)

--- pkg/generator/password-strengthtest_test.go
+++ pkg/generator/password-strengthtest_test.go
@@ -16,19 +16,37 @@ func Test_0lengthpass(t *testing.T) {
 }
 func Test_8lengthpass(t *testing.T) {
 	Loggers = logger.CreateLoggers("testlogs.txt")
-	gp := GenPassword(16, false)
+	gp := GenPassword(8, false)
 	ps := Passwordstren(gp)
 
-	if ps >= 0 {
-		t.Errorf("Expected Strength 1 of password strength %q", ps)
+	if ps != 2 {
+		t.Errorf("Expected Strength 2 of password strength %q", ps)
 	}
 }
 func Test_16lengthpass(t *testing.T) {
 	Loggers = logger.CreateLoggers("testlogs.txt")
 	gp := GenPassword(16, false)
 	ps := Passwordstren(gp)
+	println(ps)
+	if ps != 2 {
+		t.Errorf("Expected Strength 20 of password strength %q", ps)
+	}
+}
+func Test_8lengthpassWithSpecial(t *testing.T) {
+	Loggers = logger.CreateLoggers("testlogs.txt")
+	gp := GenPassword(8, true)
+	ps := Passwordstren(gp)
 
-	if ps >= 0 {
-		t.Errorf("Expected Strength 1 of password strength %q", ps)
+	if ps != 4 {
+		t.Errorf("Expected Strength 2 of password strength %q", ps)
+	}
+}
+func Test_16lengthpassWithSpecial(t *testing.T) {
+	Loggers = logger.CreateLoggers("testlogs.txt")
+	gp := GenPassword(16, true)
+	ps := Passwordstren(gp)
+	println(ps)
+	if ps != 4 {
+		t.Errorf("Expected Strength 20 of password strength %q", ps)
 	}
 }
