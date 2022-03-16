a124c156dcb76c2525d06b6dad1e7f971d4c38ad
hunter-holton <hunter.holton@me.com>
2022-03-16 14:11:56 -0600

    tutorial runs added necissary dependencies in comments

--- clients/skel.go
+++ clients/skel.go
@@ -1,6 +1,6 @@
 //go get -u github.com/gin-gonic/gin run this to get gin on dev machine
 // tutorial provided by https://go.dev/doc/tutorial/web-service-gin
-
+//also need this dependency go get -u github.com/gin-gonic/contrib/static
 package main
 
 import (
