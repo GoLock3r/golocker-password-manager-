096f863416a0847db4d57aba42d68cf15d42a567
hunter-holton <hunter.holton@me.com>
2022-04-21 14:26:34 -0600

    fixed test for password strenth testing

--- pkg/generator/password-strengthtest_test.go
+++ pkg/generator/password-strengthtest_test.go
@@ -28,7 +28,7 @@ func Test_16lengthpass(t *testing.T) {
 	gp := GenPassword(16, false)
 	ps := Passwordstren(gp)
 	if ps != 2 {
-		t.Errorf("Expected Strength 20 of password strength %q", ps)
+		t.Errorf("Expected Strength 2 of password strength %q", ps)
 	}
 }
 func Test_8lengthpassWithSpecial(t *testing.T) {
@@ -36,15 +36,15 @@ func Test_8lengthpassWithSpecial(t *testing.T) {
 	gp := GenPassword(8, true)
 	ps := Passwordstren(gp)
 
-	if ps != 4 {
-		t.Errorf("Expected Strength 2 of password strength %q", ps)
+	if ps < 3 {
+		t.Errorf("Expected Strength of at least 3 got %q", ps)
 	}
 }
 func Test_16lengthpassWithSpecial(t *testing.T) {
 	Loggers = logger.CreateLoggers("testlogs.txt")
 	gp := GenPassword(16, true)
 	ps := Passwordstren(gp)
-	if ps != 4 {
-		t.Errorf("Expected Strength 20 of password strength %q", ps)
+	if ps < 3 {
+		t.Errorf("Expected Strength of at least 3 got %q", ps)
 	}
 }
