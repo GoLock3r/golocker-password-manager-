7cd1a25224c96253332c7596648f16007a07e252
hunter-holton <hunter.holton@me.com>
2022-04-24 18:46:48 -0600

    added skeleton code for tests and tested all display only methods, all working if ran individually if ran together they fail

--- pkg/web/web_test.go
+++ pkg/web/web_test.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strings"
 	"testing"
+	"golock3r/server/db"
 )
 
 func TestLandingPage(t *testing.T) {
@@ -47,4 +48,100 @@ func TestCreateUser(t *testing.T) {
 	
 
 	
+}
+func TestLogout(t *testing.T) {
+	db.Connect("demo")
+	os.Chdir("../")
+	req := httptest.NewRequest(http.MethodGet, "/logout", nil)
+	w := httptest.NewRecorder()
+	var logout = logout(w,req)
+	if !logout {
+		t.Error("logout unseccessful")
+	}
+}
+func TestReadall(t *testing.T) {
+	os.Chdir("../")
+	req := httptest.NewRequest(http.MethodGet, "/home/display", nil)
+	w := httptest.NewRecorder()
+	var readAll = readAll(w,req)
+	if !readAll{
+		t.Error("unable to read all")
+
+	}
+}
+func TestSearchByTitle(t *testing.T){
+	os.Chdir("../")
+	req := httptest.NewRequest(http.MethodGet, "/home/searchTitle", nil)
+	w := httptest.NewRecorder()
+	var landingpage = searchByTitle(w, req)
+	if !landingpage {
+		t.Error("expected to have the search by title page shown")
+	}
+
+}
+func TestSearchByTitleSubmit(t *testing.T){
+
+}
+func TestSearchByUsername(t *testing.T){
+	os.Chdir("../")
+	req := httptest.NewRequest(http.MethodGet, "/home/searchUser", nil)
+	w := httptest.NewRecorder()
+	var landingpage = searchByUsername(w, req)
+	if !landingpage {
+		t.Error("expected to have the search by username page shown")
+	}
+}
+func TestSearchByUsernameSubmit(t *testing.T){
+
+}
+
+func TestDelete(t *testing.T){
+	os.Chdir("../")
+	req := httptest.NewRequest(http.MethodGet, "/home/delete", nil)
+	w := httptest.NewRecorder()
+	var landingpage = login(w, req)
+	if !landingpage {
+		t.Error("expected to have the delete page shown")
+	}
+}
+
+func TestDeleteSubmit(t *testing.T){
+
+}
+
+func TestCreateEntry(t *testing.T){
+	os.Chdir("../")
+	req := httptest.NewRequest(http.MethodGet, "/home/create", nil)
+	w := httptest.NewRecorder()
+	var landingpage = login(w, req)
+	if !landingpage {
+		t.Error("expected to have the create entry page shown")
+	}
+}
+
+func TestCreateEntrySubmit(t* testing.T){
+
+}
+func TestEdit(t *testing.T){
+	os.Chdir("../")
+	req := httptest.NewRequest(http.MethodGet, "/home/edit", nil)
+	w := httptest.NewRecorder()
+	var landingpage = login(w, req)
+	if !landingpage {
+		t.Error("expected to have the edit page didnt show ")
+	}
+}
+
+func TestEditSubmit(t *testing.T){
+
+}
+
+func TestHome(t *testing.T){
+	os.Chdir("../")
+	req := httptest.NewRequest(http.MethodGet, "/home", nil)
+	w := httptest.NewRecorder()
+	var landingpage = home(w, req)
+	if !landingpage {
+		t.Error("expected to have the home page shown landing page didnt show ")
+	}
 }
