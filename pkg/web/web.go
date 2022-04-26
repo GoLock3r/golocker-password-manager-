c6a3a7b00c61d43a881b1e0745dc7da71b72b0fb
hunter-holton <hunter.holton@me.com>
2022-04-25 20:49:59 -0600

    our front end works

--- pkg/web/web.go
+++ pkg/web/web.go
@@ -24,7 +24,7 @@ var key []byte
 // Serve a login page to the user and pass credentials off to the
 // loginSubmit function to verify these credentials
 func login(w http.ResponseWriter, r *http.Request) bool {
-	os.Chdir("assets/")
+	os.Chdir("web/assets/")
 	var fileName = "login.html"
 	t, err := template.ParseFiles(fileName)
 	if err != nil {
@@ -42,7 +42,7 @@ func login(w http.ResponseWriter, r *http.Request) bool {
 // Process user credentials given from the login function.
 // Utilizes authtool package functionality to validate credentials.
 func loginSubmit(w http.ResponseWriter, r *http.Request) bool {
-	os.Chdir("assets/")
+	os.Chdir("web/assets/")
 	loggers := logger.CreateLoggers("testlogs.txt")
 	authtool.Loggers = loggers
 	authtool.LoginFile = "logins.txt"
@@ -79,7 +79,7 @@ func loginSubmit(w http.ResponseWriter, r *http.Request) bool {
 
 // Strip validation from the users database and dissconects from said data base\
 func logout(w http.ResponseWriter, r *http.Request) bool {
-	os.Chdir("assets/")
+	os.Chdir("web/assets/")
 	loggers := logger.CreateLoggers("testlogs.txt")
 	authtool.Loggers = loggers
 	if validated {
@@ -180,7 +180,7 @@ func readAll(w http.ResponseWriter, r *http.Request) bool {
 // Searches entry titles and usernames and displays the results
 // for a validated user
 func searchByTitle(w http.ResponseWriter, r *http.Request) bool {
-	os.Chdir("assets/")
+	os.Chdir("web/assets/")
 	if validated {
 
 		var fileName = "searchTitle.html"
@@ -202,7 +202,7 @@ func searchByTitle(w http.ResponseWriter, r *http.Request) bool {
 	}
 }
 func searchByTitle_submit(w http.ResponseWriter, r *http.Request) {
-	os.Chdir("assets/")
+	os.Chdir("web/assets/")
 	if validated {
 
 		var display = ""
@@ -225,7 +225,7 @@ func searchByTitle_submit(w http.ResponseWriter, r *http.Request) {
 }
 
 func searchByUsername(w http.ResponseWriter, r *http.Request) bool {
-	os.Chdir("assets/")
+	os.Chdir("web/assets/")
 	if validated {
 		var fileName = "searchUsername.html"
 		t, err := template.ParseFiles(fileName)
@@ -246,7 +246,7 @@ func searchByUsername(w http.ResponseWriter, r *http.Request) bool {
 	}
 }
 func searchByUsername_submit(w http.ResponseWriter, r *http.Request) {
-	os.Chdir("assets/")
+	os.Chdir("web/assets/")
 	if validated {
 		var display = ""
 		var fileName = "searchByUsername.html"
@@ -269,7 +269,7 @@ func searchByUsername_submit(w http.ResponseWriter, r *http.Request) {
 
 // Deletes an entry from the database for a validated user
 func delete_submit(w http.ResponseWriter, r *http.Request) {
-	os.Chdir("assets/")
+	os.Chdir("web/assets/")
 	if validated {
 		var fileName = "delete-submit.html"
 		t, err := template.ParseFiles(fileName)
@@ -316,7 +316,7 @@ func delete(w http.ResponseWriter, r *http.Request) bool {
 // Creates a new entry to be securely stored on the database for
 // a validated user
 func createEntry(w http.ResponseWriter, r *http.Request) bool {
-	os.Chdir("assets/")
+	os.Chdir("web/assets/")
 	if validated {
 		var fileName = "create.html"
 		t, err := template.ParseFiles(fileName)
@@ -339,7 +339,7 @@ func createEntry(w http.ResponseWriter, r *http.Request) bool {
 }
 
 func createEntrySubmit(w http.ResponseWriter, r *http.Request) {
-	os.Chdir("assets/")
+	os.Chdir("web/assets/")
 	if validated {
 		entry := map[string]string{
 			"title":        r.FormValue("title"),
@@ -390,7 +390,7 @@ func edit(w http.ResponseWriter, r *http.Request) bool {
 }
 
 func edit_submit(w http.ResponseWriter, r *http.Request) {
-	os.Chdir("assets/")
+	os.Chdir("web/assets/")
 	if validated {
 		db.UpdateEntry(r.FormValue("title"), r.FormValue("update_key"), r.FormValue("update_value"))
 		if validated {
@@ -413,7 +413,7 @@ func edit_submit(w http.ResponseWriter, r *http.Request) {
 
 // Display the homepage for a validated user
 func home(w http.ResponseWriter, r *http.Request) bool {
-	os.Chdir("assets/")
+	os.Chdir("web/assets/")
 	if validated {
 		w.WriteHeader(http.StatusOK)
 		var fileName = "home.html"
